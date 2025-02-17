e0e476a162cc283993363c5cee8ec44983682a86
Santos <riansantoso3116@gmail.com>
2025-02-17 08:41:33 +0700

    commit-8 bootstrap icon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,18 +12,18 @@
 	<!-- Toastr CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.css" />
 	<link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.css" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-	
+
 
 	<!-- Local Stylesheets -->
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="<?= base_url('assets/assets/vendors/iconly/bold.css') ?>">
 	<link rel="stylesheet" href="<?= base_url('assets/assets/vendors/perfect-scrollbar/perfect-scrollbar.css') ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/assets/vendors/bootstrap-icons/bootstrap-icons.css') ?>">
 	<link rel="stylesheet" href="<?= base_url('assets/assets/css/app.css') ?>">
 	<link rel="shortcut icon" href="<?= base_url('assets/assets/images/favicon.svg') ?>" type="image/x-icon">
 </head>
+
 <body>
 	<div id="app">
-
